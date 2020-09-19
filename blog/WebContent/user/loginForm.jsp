e0765e43df63ec8d0bbd20ebe649e3208cecf978
leesunggyun <tjdrbs1017@naver.com>
2020-09-19 16:29:07 +0900

    5. 로그인 회원가입 완료 및 팩토리 패턴 세팅 완료

--- blog/WebContent/user/loginForm.jsp
+++ blog/WebContent/user/loginForm.jsp
@@ -1,8 +1,29 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<%@ include file="../layout/header.jsp" %>
+<%@ include file="../layout/header.jsp"%>
+<br />
+<br />
+<div class="container">
+	<form action="/user?cmd=loginProc" method="post">
 
-<h1>/user/loginForm.jsp</h1>
+		<div class="form-group">
+			<label>유저네임:</label> 
+			<input type="text" class="form-control" placeholder="Enter username"  name="username"/>
+		</div>
 
-<%@ include file="../layout/footer.jsp" %>
+		<div class="form-group">
+			<label>패스워드:</label> 
+			<input type="password" class="form-control" placeholder="Enter password"  name="password"/>
+		</div>
+
+		<div class="form-group form-check">
+			<label class="form-check-label"> 
+			<input class="form-check-input" type="checkbox"> Remember me
+			</label>
+		</div>
+
+		<button type="submit" class="btn btn-primary">로그인</button>
+	</form>
+</div>
+
+<%@ include file="../layout/footer.jsp"%>
---
+++ blog/src/com/cos/blog/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.cos.blog.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cos.blog.action.Action;
+import com.cos.blog.action.UserJoinFormAction;
+import com.cos.blog.action.UserJoinProcAction;
+import com.cos.blog.action.UserLoginFormAction;
+import com.cos.blog.action.UserLoginProcAction;
+import com.cos.blog.action.UserLogoutAction;
+
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UserController() {
+		super();
+	}
+
+	void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/user 요청됨");
+		String cmd = request.getParameter("cmd");
+		System.out.println("cmd : " + cmd);
+		
+		Action action = route(cmd);
+		if(action != null) action.execute(request, response);
+	}
+
+	private Action route(String cmd) {
+		if (cmd.equals("joinForm")) {
+			return new UserJoinFormAction();
+		} else if (cmd.equals("loginForm")) {
+			return new UserLoginFormAction();
+		} else if (cmd.equals("updateForm")) {
+			// 회원수정 페이지로 이동 Model로 이동 후 RequestDispatcher
+		} else if (cmd.equals("joinProc")) {
+			return new UserJoinProcAction();
+		} else if (cmd.equals("loginProc")) {
+			return new UserLoginProcAction();
+		} else if (cmd.equals("updateProc")) {
+			// 1. 회원수정 진행 (update) Model로 이동
+			// 2. 메인 페이지 이동 Redirect
+		} else if (cmd.equals("logout")) {
+			return new UserLogoutAction();
+		}
+		return null;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+
+}
